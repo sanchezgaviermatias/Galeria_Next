eb9911d6ea2991107c7be0369b22b6637d1e9482
Matias Sanchez <sanchezgaviermatias@gmail.com>
2022-04-23 21:48:36 -0300

    add cards section

---
+++ components/cards/BlackImageCard.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+export default function blackImageCard() {
+  const srcImagen =
+    "https://www.lifeder.com/wp-content/uploads/2022/01/montana.jpg";
+
+  return (
+    <div className="max-w-md  bg-white rounded-lg border border-gray-200 shadow-md ">
+      <a href="#">
+        <img className="rounded-t-lg" src={srcImagen} alt="" />
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+            Noteworthy technology acquisitions 2021
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 ">
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+        <a
+          href="#"
+          className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+        >
+          Read more
+          <svg
+            className="ml-2 -mr-1 w-4 h-4"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+}
--- pages/cards.jsx
+++ pages/cards.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import BlackImageCard from "../components/cards/BlackImageCard";
 
 export default function Cards() {
-  return;
+  return (
+    <div className="max-w-4xl mx-auto">
+      <h1 className="text-center text-4xl underline underline-offset-4 decoration-red-500  mt-2 mb-6">
+        {" "}
+        Cards
+      </h1>
+
+      <div className="flex items-center ">
+        <BlackImageCard />
+        <AiOutlineArrowLeft size={100} className="mx-12  " />
+        <div className="border w-96 border-slate-50 relative shadow-lg  p-8 rounded-2xl  ">
+          <span className="absolute text-xs -top-4 bg-black text-white px-2 py-1 rounded-lg right-0  transform rotate-6">
+            Explanation
+          </span>
+          <p>En esta carda hacemos loocurass</p>
+        </div>
+        <div />
+      </div>
+    </div>
+  );
 }
